785e335637c51135c90052bb2389d660596ddf39
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-16 14:14:07 +0500

    done

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 let MarksOfSub1 = +prompt("enter your first subject number")
-let MarksOfSub1 = +prompt("enter your first subject number")
-let MarksOfSub1 = +prompt("enter your first subject number")
-let MarksOfSub1 = +prompt("enter your first subject number")
-let MarksOfSub1 = +prompt("enter your first subject number")
+let MarksOfSub2 = +prompt("enter your second subject number")
+let MarksOfSub3 = +prompt("enter your third subject number")
+let MarksOfSub4 = +prompt("enter your fourth subject number")
+let MarksOfSub5 = +prompt("enter your fifth subject number")
+
+let SumOfAllSubjectsNumber = MarksOfSub1 + MarksOfSub1 + MarksOfSub1 + MarksOfSub1 + MarksOfSub1
+let PercentageOfAllSubjects = SumOfAllSubjectsNumber /500 *100
+
+document.write (PercentageOfAllSubjects)
+
+if (PercentageOfAllSubjects >= 90){
+    document.write("A+")
+}else if (PercentageOfAllSubjects >= 80){
+    document.write("A")
+}else if (PercentageOfAllSubjects >= 70){
+    document.write("B")
+}else if (PercentageOfAllSubjects >= 60){
+    document.write("c")
+}else if (PercentageOfAllSubjects <= 60){
+    document.write("Fail")
+}
